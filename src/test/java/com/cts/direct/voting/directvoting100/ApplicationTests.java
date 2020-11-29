709c76fabcd35605050c9ca699fe564618560e6d
Gaurav Tomar <gauravworld.07@gmail.com>
2020-11-29 17:17:10 +0530

    Add files via upload
    Spring-boot application pushed.

---
+++ src/test/java/com/cts/direct/voting/directvoting100/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cts.direct.voting.directvoting100;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
